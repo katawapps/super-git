b30005d79f3b8f87a6d8990909ef3d61b351fdd3
emanuel estrella <bryanrengif@gmail.com>
2016-03-03 16:31:27 -0500

    mañuco

--- index.php
+++ index.php
@@ -8,8 +8,7 @@
 </head>
 <body>
 		<h1>MI NOMBRE ES: HUGO</h1>
-		<h1>MI NOMBRE ES: QUE LECHES!!!("ALFREDO")</h1>
-		<h1>Mi Nombre Es Frank York de la Selva xD</h1>
-		<h1>MI NOMBRE ES: ISMAEL FERNANDEZ </h1>
+		
+
 </body>
 </html>
